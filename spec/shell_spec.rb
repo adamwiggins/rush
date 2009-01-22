dbc2eb38b93cb8b724b5c05d5fa18e8653f7b241
Martin Kuehl <martin.kuehl@gmail.com>
2009-01-22 13:07:09 +0100

    • Make specs pass for changed completion functions. • Add specs for added completion use cases.

--- spec/shell_spec.rb
+++ spec/shell_spec.rb
@@ -7,6 +7,15 @@ describe Rush::Shell do
 	end
 
 	it "matches open path commands for readline tab completion" do
-		@shell.path_parts("dir['app").should == [ "dir", "'", "app", "" ]
+		@shell.path_parts("dir['app").should == [ "dir", "[", "'", "app", "" ]
+		@shell.path_parts('dir/"app').should == [ "dir", "/", '"', "app", "" ]
+	end
+	it "matches open path commands on globals for readline tab completion" do
+		@shell.path_parts("$dir['app").should == [ "$dir", "[", "'", "app", "" ]
+		@shell.path_parts('$dir/"app').should == [ "$dir", "/", '"', "app", "" ]
+	end
+	it "matches open path commands on instance vars for readline tab completion" do
+		@shell.path_parts("@dir['app").should == [ "@dir", "[", "'", "app", "" ]
+		@shell.path_parts('@dir/"app').should == [ "@dir", "/", '"', "app", "" ]
 	end
 end
