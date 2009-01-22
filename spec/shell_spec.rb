574e3802afb84e95a1acfdb59a342a1fedcc3151
Martin Kuehl <martin.kuehl@gmail.com>
2009-01-22 14:35:08 +0100

    Formatting: insert line breaks in between specs.

--- spec/shell_spec.rb
+++ spec/shell_spec.rb
@@ -10,10 +10,12 @@ describe Rush::Shell do
 		@shell.path_parts("dir['app").should == [ "dir", "[", "'", "app", "" ]
 		@shell.path_parts('dir/"app').should == [ "dir", "/", '"', "app", "" ]
 	end
+
 	it "matches open path commands on globals for readline tab completion" do
 		@shell.path_parts("$dir['app").should == [ "$dir", "[", "'", "app", "" ]
 		@shell.path_parts('$dir/"app').should == [ "$dir", "/", '"', "app", "" ]
 	end
+
 	it "matches open path commands on instance vars for readline tab completion" do
 		@shell.path_parts("@dir['app").should == [ "@dir", "[", "'", "app", "" ]
 		@shell.path_parts('@dir/"app').should == [ "@dir", "/", '"', "app", "" ]
