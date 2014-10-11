ea222cd4e57c08c673255dcd94b6deb5422bfe16
Sergey Smagin <smaginsergey1310@gmail.com>
2014-10-11 23:35:37 +0400

    [refs #9] Rewrite autocomplete system

--- lib/rush/shell.rb
+++ lib/rush/shell.rb
@@ -1,11 +1,16 @@
+require_relative 'shell/completion'
 require 'coolline'
 require 'coderay'
 require 'pp'
 
-# Rush::Shell is used to create an interactive shell.  It is invoked by the rush binary.
 module Rush
+  # Rush::Shell is used to create an interactive shell.
+  # It is invoked by the rush binary.
+  #
   class Shell
-    attr_accessor :suppress_output, :config, :history
+    include Rush::Completion
+
+    attr_accessor :suppress_output, :config, :history, :pure_binding
     # Set up the user's environment, including a pure binding into which
     # env.rb and commands.rb are mixed.
     def initialize
@@ -14,23 +19,20 @@ module Rush
       pwd = Rush::Dir.new(ENV['PWD']) if ENV['PWD']
 
       @config = Rush::Config.new
-
       @history = Coolline::History.new config.history_file.full_path
-
       @readline = Coolline.new do |c|
-        c.transform_proc = proc { syntax_highlight c.line }
+        c.transform_proc  = proc { syntax_highlight c.line }
         c.completion_proc = proc { complete c.completed_word }
       end
 
       @box = Rush::Box.new
-      @pure_binding = @box.instance_eval "binding"
+      @pure_binding = @box.instance_eval 'binding'
       $last_res = nil
 
       eval config.load_env, @pure_binding
-
       commands = config.load_commands
       Rush::Dir.class_eval commands
-      Array.class_eval commands
+      Array.class_eval     commands
 
       # Multiline commands should be stored somewhere
       @multiline_cmd = ''
@@ -40,7 +42,7 @@ module Rush
     def execute(cmd)
       res = eval(@multiline_cmd << "\n" << cmd, @pure_binding)
       $last_res = res
-      eval("_ = $last_res", @pure_binding)
+      eval('_ = $last_res', @pure_binding)
       @multiline_cmd = ''
       print_result res
     rescue SyntaxError => e
@@ -63,9 +65,8 @@ module Rush
       loop do
         prompt = self.class.prompt || "#{`whoami`.chomp} $ "
         cmd = @readline.readline prompt
-
-        finish if cmd.nil? or cmd == 'exit'
-        next if cmd.empty?
+        finish if cmd.nil? || cmd == 'exit'
+        next   if cmd.empty?
         @history << cmd
         execute cmd
       end
@@ -84,104 +85,25 @@ module Rush
     end
 
     # Nice printing of different return types, particularly Rush::SearchResults.
+    #
     def print_result(res)
-      return if self.suppress_output
-      if res.kind_of? String
+      return if suppress_output
+      if res.is_a? String
         output = res
-      elsif res.kind_of? Rush::SearchResults
+      elsif res.is_a? Rush::SearchResults
         output = res.to_s <<
-          "#{res.entries.size} matching files with #{res.lines.size} matching lines"
+          "#{res.entries.size} matching files with #{res.lines.size} lines"
       elsif res.respond_to? :each
-        pp res
+        print '   = '
+        output = pp(res)
       else
-        output = "=> #{res.inspect}"
+        output = "   = #{res.inspect}"
       end
-      output.lines.count > 5 ? output.less : puts(output)
+      output.to_s.lines.count > 5 ? output.less : puts(output)
     end
 
-    # Try to do tab completion on dir square brackets and slash accessors.
+    # Syntax highlighting with coderay.
     #
-    # Example:
-    #
-    # dir['subd  # presing tab here will produce dir['subdir/ if subdir exists
-    # dir/'subd  # presing tab here will produce dir/'subdir/ if subdir exists
-    #
-    # This isn't that cool yet, because it can't do multiple levels of subdirs.
-    # It does work remotely, though, which is pretty sweet.
-    #
-    # TODO:
-    #   1. move to separate module
-    #   2. agressive refactor it
-    #
-    def complete(input)
-      receiver, accessor, *rest = path_parts(input)
-      return [] unless receiver
-      case accessor
-      when /^[\[\/]$/ then complete_path(receiver, accessor, *rest)
-      when /^\.$/     then complete_method(receiver, accessor, *rest)
-      when nil        then complete_variable(receiver, *rest)
-      else []
-      end
-    end
-
-    # TODO: get string from space to last dot and do something sane with it.
-    # I just don't want to figure out what's going on there.
-    def path_parts(input)   # :nodoc:
-      case input
-      when /((?:@{1,2}|\$|)\w+(?:\[[^\]]+\])*)([\[\/])(['"])([^\3]*)$/
-        $~.to_a.slice(1, 4).push($~.pre_match)
-      when /((?:@{1,2}|\$|)\w+(?:\[[^\]]+\])*)(\.)(\w*)$/
-        $~.to_a.slice(1, 3).push($~.pre_match)
-      when /((?:@{1,2}|\$|)\w+)$/
-        $~.to_a.slice(1, 1).push(nil).push($~.pre_match)
-      else
-        [ nil, nil, nil ]
-      end
-    end
-
-    def complete_method(receiver, dot, partial_name, pre)
-      path = eval("#{receiver}.full_path", @pure_binding) rescue nil
-      box = eval("#{receiver}.box", @pure_binding) rescue nil
-      return [] unless (path and box)
-      box[path].methods. # why box[path] ?
-        select { |e| e.match /^#{Regexp.escape(partial_name)}/ }.
-        map    { |e| (pre || '') + receiver + dot + e.to_s }
-    end
-
-    def complete_path(possible_var, accessor, quote, partial_path, pre)   # :nodoc:
-      original_var, fixed_path = possible_var, ''
-      if /^(.+\/)([^\/]*)$/ === partial_path
-        fixed_path, partial_path = $~.captures
-        possible_var += "['#{fixed_path}']"
-      end
-      full_path = eval("#{possible_var}.full_path", @pure_binding) rescue nil
-      box = eval("#{possible_var}.box", @pure_binding) rescue nil
-      return [] unless (full_path and box)
-      Rush::Dir.new(full_path, box).entries.
-        select { |e| e.name.match(/^#{Regexp.escape(partial_path)}/) }.
-        map    { |e| (pre || '') + original_var + accessor + quote +
-          fixed_path + e.name + (e.dir? ? "/" : "") }
-    end
-
-    def complete_variable(partial_name, pre)
-      pre = eval(pre, @pure_binding) rescue nil
-      the_binding = pre ? pre.instance_eval('binding') : @pure_binding
-      lvars = eval('local_variables', the_binding)
-      gvars = eval('global_variables', the_binding)
-      ivars = eval('instance_variables', the_binding)
-      mets = eval('methods', the_binding) || eval('Kernel.methods')
-      consts = eval('Object.constants', the_binding)
-      (executables + lvars + gvars + ivars + mets + consts).
-        select { |e| e.match(/^#{Regexp.escape(partial_name)}/) }.
-        map    { |e| (pre || '') + e.to_s }
-    end
-
-    def executables
-      ENV['PATH'].split(':').
-        map { |x| Rush::Dir.new(x).entries.map(&:name) }.
-        flatten
-    end
-
     def syntax_highlight(input)
       CodeRay.encode input, :ruby, :term
     end
