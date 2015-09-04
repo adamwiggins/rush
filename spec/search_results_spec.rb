b438f75042261ecb38f4aba6dfc6fbd2c7a73540
Joel McCracken <mccracken.joel@gmail.com>
2015-09-03 21:36:32 -0400

    formatting

--- spec/search_results_spec.rb
+++ spec/search_results_spec.rb
@@ -1,44 +1,44 @@
 require File.dirname(__FILE__) + '/base'
 
 describe Rush::SearchResults do
-	before do
-		@results = Rush::SearchResults.new(/pat/)
-		@file = Rush::File.new("file")
-	end
+  before do
+    @results = Rush::SearchResults.new(/pat/)
+    @file = Rush::File.new("file")
+  end
 
-	it "returns its list of entries" do
-		@results.add(@file, %w(a))
-		@results.entries.should == [ @file ]
-	end
+  it "returns its list of entries" do
+    @results.add(@file, %w(a))
+    @results.entries.should == [ @file ]
+  end
 
-	it "only returns each entry once no matter how many line matches it has" do
-		@results.add(@file, %w(a b))
-		@results.entries.should == [ @file ]
-	end
+  it "only returns each entry once no matter how many line matches it has" do
+    @results.add(@file, %w(a b))
+    @results.entries.should == [ @file ]
+  end
 
-	it "returns its list of matched lines" do
-		@results.add(@file, %w(a b))
-		@results.lines.should == %w(a b)
-	end
+  it "returns its list of matched lines" do
+    @results.add(@file, %w(a b))
+    @results.lines.should == %w(a b)
+  end
 
-	it "returns all lines for each entry in a flattened form" do
-		file2 = Rush::File.new("another file")
-		@results.add(@file, %w(a b))
-		@results.add(file2, %w(c d))
-		@results.lines.should == %w(a b c d)
-	end
+  it "returns all lines for each entry in a flattened form" do
+    file2 = Rush::File.new("another file")
+    @results.add(@file, %w(a b))
+    @results.add(file2, %w(c d))
+    @results.lines.should == %w(a b c d)
+  end
 
-	it "returns a hash of entries_with_lines" do
-		@results.add(@file, %w(a))
-		@results.entries_with_lines.should == { @file => %w(a) }
-	end
+  it "returns a hash of entries_with_lines" do
+    @results.add(@file, %w(a))
+    @results.entries_with_lines.should == { @file => %w(a) }
+  end
 
-	it "mixes in Commands to operate like a dir or entry array" do
-		@results.methods.include?("search").should be_true
-	end
+  it "mixes in Commands to operate like a dir or entry array" do
+    @results.methods.include?("search").should be_true
+  end
 
-	it "mixes in Enumerable to behave like an array" do
-		@results.add(@file, %w(a))
-		@results.map { |e| e }.should == [ @file ]
-	end
+  it "mixes in Enumerable to behave like an array" do
+    @results.add(@file, %w(a))
+    @results.map { |e| e }.should == [ @file ]
+  end
 end
